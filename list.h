fd20a0de878ec2a69f4875dbda204584d8f4cc64
shurikenator <33102322+shurikenator@users.noreply.github.com>
2017-11-23 21:34:08 +0300

    Update list.h

--- list.h
+++ list.h
@@ -4,10 +4,27 @@ using namespace std;
 
 class IntList
 {
+private:
+	struct ListItem {
+		int item;
+		ListItem *next;
+
+		ListItem(int i, ListItem *n = NULL)
+		{
+			item = i;
+			next = n;
+		}
+	};
+	int itemsCount;
+	int pos;
+	int val;
+	ListItem  *first;
+	ListItem *last;
 public:
 	IntList()
 	{
 		itemsCount = 0;
+		
 		first = last = NULL;
 	}
 
@@ -36,27 +53,24 @@ public:
 	void AddLast(int item);
 	int RemoveFirst();
 	bool Remove(int n);
-	void Insert(int n);
-
+	void Insert_1(int n);
+	size_t get_size() const;					//получить размер списка
+	bool is_empty(ListItem *&head);					//возвращает true, если список пуст
+	int & operator[](int i);					//неконстантный оператор - получение ссылки на i-ый элемент
+	const int & operator[](int i) const;				//константный оператор - получение ссылки на i-ый элемент
+	void pop_front();						//удаление элемента из начала списка
+	int & get_front();						//получение первого элемента
+	const int & get_front() const;					//
+	void insert(ListItem *&head,int pos, int val);			//добавление нового элемента в заданную позицию
+	void erase(ListItem *&head, int pos);				//удалить определенный элемент
+	int get_list(ListItem * head);					//вывести список
 	int GetItemsCount()
 	{
 		return itemsCount;
 	}
 	string GetAllItemsInfo();
 
-private:
-	struct ListItem{
-		int item;
-		ListItem *next;
 
-		ListItem(int i, ListItem *n = NULL)
-		{
-			item = i;
-			next = n;
-		}
-	};
-	int itemsCount;
-	ListItem  *first;
-	ListItem *last;
 };
+
 
